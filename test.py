6ddd66b961aca4c626c5504ea9132f65567065d6
AkashChaudhare <akash.chaudhare@gmail.com>
2023-06-01 07:16:08 +0530

    a

--- test.py
+++ test.py
@@ -1,52 +1,43 @@
-from multiprocessing import Process, Manager
+from multiprocessing import Process, Manager, Lock
 
-def parallel_min(data, shared_result):
-    min_val = min(data)
-    shared_result.append(min_val)
+def dfs(graph, node, visited, lock):
+    visited.add(node)
 
-def parallel_max(data, shared_result):
-    max_val = max(data)
-    shared_result.append(max_val)
+    for next_node in graph[node]:
+        if next_node not in visited:
+            dfs(graph, next_node, visited, lock)
 
-def parallel_sum(data, shared_result):
-    sum_val = sum(data)
-    shared_result.append(sum_val)
+def para_dfs(graph, node, shared_list, lock):
+    print('Executing parallel dfs')
+    visited = set()
 
-def parallel_average(data, shared_result):
-    avg_val = sum(data) / len(data)
-    shared_result.append(avg_val)
+    dfs(graph, node, visited, lock)
+
+    with lock:
+        shared_list.extend(visited)
 
 if __name__ == '__main__':
-    input_data = [4, 8, 2, 1, 6, 7, 5, 3]
+    graph = {
+        'A': ['B', 'C'],
+        'B': ['A', 'D'],
+        'C': ['A', 'E'],
+        'D': ['B'],
+        'E': ['C']
+    }
+
+    root = 'A'
 
     with Manager() as manager:
-        shared_result = manager.list()
-
+        shared_list = manager.list()
+        lock = Lock()
         process_list = []
 
-        min_process = Process(target=parallel_min, args=(input_data, shared_result))
-        min_process.start()
-        process_list.append(min_process)
-
-        max_process = Process(target=parallel_max, args=(input_data, shared_result))
-        max_process.start()
-        process_list.append(max_process)
-
-        sum_process = Process(target=parallel_sum, args=(input_data, shared_result))
-        sum_process.start()
-        process_list.append(sum_process)
-
-        average_process = Process(target=parallel_average, args=(input_data, shared_result))
-        average_process.start()
-        process_list.append(average_process)
+        
+        search_process = Process(target=para_dfs, args=(graph, root, shared_list, lock))
+        search_process.start()
+        process_list.append(search_process)
 
         for process in process_list:
             process.join()
 
-        result = list(shared_result)
-        min_val, max_val, sum_val, avg_val = result
-
-        print("Minimum:", min_val)
-        print("Maximum:", max_val)
-        print("Sum:", sum_val)
-        print("Average:", avg_val)
+        print(list(shared_list))
